2b9db5b8746b10cd3594fec07618341c2751019a
Adam <adam@ineedroot.com>
2025-10-17 21:59:58 +0100

    Convert UI from .ui files to pure Python
    - Remove Qt Designer .ui files (gui.ui, about.ui)
    - Update generated Python files to be manually maintained
    - Remove auto-generation warnings and add proper docstrings
    - Update About dialog branding from Asgard to Bifrost
    - UI now fully editable in code without Qt Designer dependency

--- about.py
+++ about.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
+"""
+Bifrost About Dialog UI Definition
 
-# Form implementation generated from reading ui file 'about.ui'
-#
-# Created by: PyQt5 UI code generator 5.4.1
-#
-# WARNING! All changes made in this file will be lost!
+This file defines the About dialog UI for the Bifrost application.
+Originally generated from a Qt Designer .ui file, now maintained manually.
+"""
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
@@ -24,24 +24,21 @@ class Ui_Dialog(object):
 
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
-        Dialog.setWindowTitle(_translate("Dialog", "About ..."))
+        Dialog.setWindowTitle(_translate("Dialog", "About Bifrost"))
         self.AboutContent.setHtml(_translate("Dialog", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
 "</style></head><body style=\" font-family:\'MS Shell Dlg 2\'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
-"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:16pt; font-weight:600; text-decoration: underline;\">Asgard</span></p>\n"
-"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:12pt;\">A Graphical User Interface for Thor</span></p>\n"
+"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:16pt; font-weight:600; text-decoration: underline;\">Bifrost</span></p>\n"
+"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:12pt;\">RepRap Firmware Control Interface for Thor</span></p>\n"
 "<p align=\"center\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:12pt;\"><br /></p>\n"
 "<p align=\"center\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:12pt;\"><br /></p>\n"
-"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:10pt;\">Asgard source files: </span><a href=\"https://github.com/AngelLM/Asgard\"><span style=\" font-size:10pt; text-decoration: underline; color:#0000ff;\">Asgard\'s GitHub repository</span></a></p>\n"
+"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:10pt;\">Based on Asgard by </span><a href=\"http://www.angel-lm.com\"><span style=\" font-size:10pt; text-decoration: underline; color:#0000ff;\">Ángel L.M.</span></a></p>\n"
 "<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:10pt;\">More info about Thor: </span><a href=\"https://hackaday.io/project/12989-thor\"><span style=\" font-size:10pt; text-decoration: underline; color:#0000ff;\">Thor\'s Hackaday Project</span></a></p>\n"
 "<p align=\"center\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:8pt; color:#000000;\"><br /></p>\n"
-"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:10pt;\">If you are interested in this project consideer joining us at </span><a href=\"https://groups.google.com/forum/#!forum/thor-opensource-3d-printable-robotic-arm\"><span style=\" font-size:10pt; text-decoration: underline; color:#0000ff;\">Thor\'s community</span></a></p>\n"
+"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:10pt;\">Join the Thor community at </span><a href=\"https://groups.google.com/forum/#!forum/thor-opensource-3d-printable-robotic-arm\"><span style=\" font-size:10pt; text-decoration: underline; color:#0000ff;\">Thor\'s community forum</span></a></p>\n"
 "<p align=\"center\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:10pt;\"><br /></p>\n"
-"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:8pt;\">All files included in this project are licensed under a </span><a href=\"https://creativecommons.org/licenses/by-sa/4.0/\"><span style=\" font-size:8pt; text-decoration: underline; color:#0000ff;\">Creative Commons Attribution-ShareAlike 4.0 International License </span></a></p>\n"
-"<p align=\"center\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:8pt; text-decoration: underline; color:#0000ff;\"><br /></p>\n"
-"<p align=\"center\" style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:8pt; text-decoration: underline; color:#0000ff;\"><br /></p>\n"
-"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Created by <a href=\"http://www.angel-lm.com\"><span style=\" font-size:8pt; text-decoration: underline; color:#0000ff;\">Ángel L.M.</span></a></p></body></html>"))
+"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:8pt;\">Licensed under </span><a href=\"https://creativecommons.org/licenses/by-sa/4.0/\"><span style=\" font-size:8pt; text-decoration: underline; color:#0000ff;\">Creative Commons Attribution-ShareAlike 4.0 International License</span></a></p></body></html>"))
 
 
 if __name__ == "__main__":
